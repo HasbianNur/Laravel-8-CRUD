15df93815a5c499716f55b8a4c093c3c19ea0404
HasbianNur <kitaraja600@gmail.com>
2023-02-20 08:31:21 +0800

    first commit

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post; // load Post model
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+}
